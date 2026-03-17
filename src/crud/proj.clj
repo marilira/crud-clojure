e7e3a98d857ffc9a40ea383c6961333ee3bc9b92
Mariana Lira <marilira2006@gmail.com>
2026-03-16 21:26:44 -0300

    feat: add stop-server function

--- src/crud/proj.clj
+++ src/crud/proj.clj
@@ -1,6 +1,14 @@
 (ns crud.proj
   (:require [org.httpkit.server :as server]))
 
+(defonce server (atom nil))
+
+(defn stop-server []
+  (when @server
+    (@server :timeout 100)
+    (reset! server nil))
+  (println "Server terminated"))
+
 (defn app [req]
   {:status 200
    :headers {"Content-Type" "text/plain"}
@@ -8,5 +16,5 @@
 
 (defn -main [& args]
   (let [port 8080]
-    (server/run-server app {:port port})
+    (reset! server (server/run-server #'app {:port port}))
     (println "Server started on port" port)))
