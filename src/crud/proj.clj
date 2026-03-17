bdc6dcc7ac5d35bb2f86860598de9f4276d475d9
Mariana Lira <marilira2006@gmail.com>
2026-03-16 21:29:21 -0300

    feat: add POST route

--- src/crud/proj.clj
+++ src/crud/proj.clj
@@ -10,10 +10,28 @@
     (reset! server nil))
   (println "Server terminated"))
 
+(defonce db (atom {}))
+
+(defn product-handler [req]
+  (let [qs (:query-string req)
+        params (codec/form-decode qs)
+        new-id (inc (count @db))]
+    (swap! db assoc new-id params)
+    {:status 201
+     :headers {"Content-Type" "application/json"}
+     :body (str "Novo produto " new-id " - " params)})
+  )
+
 (defn app [req]
-  {:status 200
-   :headers {"Content-Type" "text/plain"}
-   :body "Hello World"})
+  (case (:request-method req)
+    :get {:status 200
+          :headers {"Content-Type" "text/plain"}
+          :body "Hello World"}
+    :post (if (= (:uri req) "/product")
+            (product-handler req)
+            {:status 405 :body "Método não permitido"})
+    {:status 404 :body "Não encontrado"}
+    ))
 
 (defn -main [& args]
   (let [port 8080]
