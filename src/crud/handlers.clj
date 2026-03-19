5ead2213a252456b1fa1f5a10728063e35041e90
Mariana Lira <marilira2006@gmail.com>
2026-03-19 10:34:24 -0300

    fix: DELETE now returns not found status

--- src/crud/handlers.clj
+++ src/crud/handlers.clj
@@ -27,11 +27,15 @@
   )
 
 (defn delete-product [id]
-  (db/delete-item id)
-  {:status 200
-     :headers {"Content-Type" "text/plain"}
-     :body (str "Produto " id " foi removido")}
-  )
+  (if (contains? (db/list-items) id)
+    (do
+      (db/delete-item id)
+      {:status 200
+       :headers {"Content-Type" "text/plain"}
+       :body (str "Produto " id " foi removido")})
+    {:status 404
+       :headers {"Content-Type" "text/plain"}
+       :body "Produto não existe"}))
 
 (defn get-product [id]
   (let [data (db/get-item id)]
